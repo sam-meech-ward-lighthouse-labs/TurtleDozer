9967b61b5f9a1608761d4235b525e93b9a40f56d
Sam Meech-Ward <sam@meech-ward.com>
2018-08-22 13:16:27 -0700

    Create a turtle class

---
+++ TurtleDozer/Turtle.swift
@@ -0,0 +1,12 @@
+//
+//  Turtle.swift
+//  TurtleDozer
+//
+//  Created by Sam Meech-Ward on 2018-08-22.
+//  Copyright © 2018 meech-ward. All rights reserved.
+//
+
+import UIKit
+
+class Turtle: NSObject {
+}
